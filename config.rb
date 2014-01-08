da40aca50d8bac9e375056e14fc3c5970668f92c
John Quealy <john@urbildpunkt.com>
2014-01-08 13:13:38 -0800

    Site Update with Middleman.

--- config.rb
+++ config.rb
@@ -1,26 +1,86 @@
-require 'zurb-foundation'
-# Require any additional compass plugins here.
+######################################################################
+# General settings.
+######################################################################
+
+# Meta.
+set :meta, {
+  title: 'Foo',
+  url: 'http://foo.com',
+}
+
+# Syntax highlighting.
+activate :syntax
+set :syntax_theme, Rouge::Themes::Base16
+
+# Change to your Google Analytics key (e.g. UA-XXXXXXXX-Y).
+# To disable GA, leave unset or set to nil
+# Code will only be injected in build environment
+# TODO: extract to MM extension
+set :ga_key, 'UA-XXXXXXX-Y'
+
+# Change to your Disqus shortname.
+# To Disable Disqus, leave unset or set to nil
+# TODO: extract to MM extension
+set :disqus_shortname, nil
+
+# Locations.
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+set :partials_dir, 'partials'
+
+after_configuration do
+  # Add bower's directory to sprockets asset path.
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
+# Ignores.
+ignore '/calendar.html'
 
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+######################################################################
+# Blog settings.
+######################################################################
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+Time.zone = "Amsterdam"
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+activate :blog do |blog|
+  blog.prefix = 'blog'
+  blog.permalink = '{year}/{title}.html'
+  blog.taglink = "tags/{tag}.html"
+  blog.default_extension = ".md"
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+  blog.paginate = true
+  blog.per_page = 10
+end
+
+# Enable XML feed. Don't forget to edit feed.xml.builder first.
+# page "/feed.xml", layout: false
 
 
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+######################################################################
+# Development environment.
+######################################################################
+
+configure :development do
+  activate :livereload, apply_js_live: false, apply_css_live: false
+end
+
+
+######################################################################
+# Build environment.
+######################################################################
+
+configure :build do
+  activate :relative_assets
+  activate :directory_indexes
+  activate :asset_hash
+
+  # Ensmallen assets
+  activate :minify_html, remove_input_attributes: false
+  activate :minify_css
+  activate :minify_javascript
+  activate :imageoptim
+end
---
+++ source/assets/javascripts/app.js
@@ -0,0 +1,51 @@
+// as the page loads, call these scripts
+jQuery(document).ready(function($) {
+
+
+	// grab an element
+	var myElement = document.querySelector("#topbar");
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(myElement, {
+	  // vertical offset in px before element is first unpinned
+	    offset : 100,
+	    // scroll tolerance in px before state changes
+	    tolerance : 50,
+	    // css classes to apply
+	    classes : {
+	        // when element is initialised
+	        initial : "headroom",
+	        // when scrolling up
+	        pinned : "headroom--pinned",
+	        // when scrolling down
+	        unpinned : "headroom--unpinned"
+	    }
+	});
+	// initialise
+	headroom.init(); 
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 20) {
+		    $('.label').fadeOut();
+		    $('.tagline').fadeOut();
+		    $("#topbar").css("background", "#ffffff");
+		    $("#topbar").css("z-index", "900");
+		} else {
+		    $('.label').fadeIn();
+		    $('.tagline').fadeIn();
+		    $("#topbar").css("background", "rgba(255,255,255,0.8)");
+		    $("#topbar").css("z-index", "90");
+		}
+	});
+
+	$('#dynamo').dynamo({
+	    speed: 1000,
+	    delay: 3000,
+	    lines: ['designer.', 'developer.']
+	});
+
+	$('.project-image a').fluidbox();
+
+
+	$("a.hire-me").attr("href", "mailto:john@urbildpunkt.com")
+
+}); /* end of as page load scripts */

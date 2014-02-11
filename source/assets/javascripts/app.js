92534340b3df1f3b8474f298709eb844be2646ee
john <john@encom>
2014-02-10 23:56:19 -0800

    Added 'THE DAY WE FIGHT BACK' banner.
    Signed-off-by: john <john@encom>

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -49,3 +49,5 @@ jQuery(document).ready(function($) {
 	$("a.hire-me").attr("href", "mailto:john@urbildpunkt.com")
 
 }); /* end of as page load scripts */
+
+(function(){var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src=document.location.protocol+"//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})()

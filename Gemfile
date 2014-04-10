72cabbc5447ee42abf9021630e5b25f85e8c3863
John Quealy <john@urbildpunkt.com>
2014-04-10 14:36:55 -0700

    Slight Visual tweaks. Added Bitters and OpenGov.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
 gem "middleman"
 gem "middleman-neat"
 gem 'slim'
--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -8,11 +8,3 @@ var myElement = document.querySelector(".nav-bar");
 var headroom  = new Headroom(myElement);
 // initialise
 headroom.init(); 
-
-tabby.init({
-    toggleActiveClass: 'active', // Class added to active toggle elements
-    contentActiveClass: 'active', // Class added to active tab content areas
-    initClass: 'js-tabby', // Class added to <html> element when initiated
-    callbackBefore: function () {}, // Function that's run before tab content is toggled
-    callbackAfter: function () {} // Function that's run after tab content is toggled
-});
